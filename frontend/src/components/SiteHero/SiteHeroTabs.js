169ad992d737349ca1f737529fc64fad157af8ae
jhtong <me at joeltong dot org>
2018-05-22 22:42:59 +0800

    refactored navigation bar to have onHover highlights

--- frontend/src/components/SiteHero/SiteHeroTabs.js
+++ frontend/src/components/SiteHero/SiteHeroTabs.js
@@ -1,35 +1,68 @@
 import React, { Component } from "react";
 import { Tabs, TabList, Tab, TabLink, Icon } from "bloomer";
+import { Link } from "react-router-dom";
+
+class CustomTab extends Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(url) {
+    this.props.updateActiveTab(url);
+  }
+
+  render() {
+    const { activeTab, tab, updateActiveTab } = this.props;
+    return (
+      <Tab
+        isActive={activeTab === tab.url}
+        onClick={() => {
+          this.handleClick(tab.url);
+        }}
+      >
+        <Link to={tab.url}>{tab.label}</Link>
+      </Tab>
+    );
+  }
+}
 
 class SiteHeroTabs extends Component {
   constructor() {
     super();
+    this.state = {
+      activeTab: "",
+      tabs: [
+        { url: "/popular", label: "Popular" },
+        { url: "/men", label: "Men" },
+        { url: "/women", label: "Women" },
+        { url: "/accessories", label: "Accessories" }
+      ]
+    };
+
+    this.updateActiveTab = this.updateActiveTab.bind(this);
+  }
+
+  updateActiveTab(tabUrl) {
+    this.setState({
+      activeTab: tabUrl
+    });
   }
 
   render() {
+    const { activeTab, tabs } = this.state;
     return (
       <Tabs isAlign="centered">
         <TabList>
-          <Tab>
-            <TabLink>
-              <span>Popular</span>
-            </TabLink>
-          </Tab>
-          <Tab isActive>
-            <TabLink>
-              <span>Men</span>
-            </TabLink>
-          </Tab>
-          <Tab>
-            <TabLink>
-              <span>Women</span>
-            </TabLink>
-          </Tab>
-          <Tab>
-            <TabLink>
-              <span>Accessories</span>
-            </TabLink>
-          </Tab>
+          {tabs.map((t, i) => (
+            <CustomTab
+              key={i}
+              tab={t}
+              activeTab={activeTab}
+              updateActiveTab={this.updateActiveTab}
+            />
+          ))}
         </TabList>
       </Tabs>
     );
