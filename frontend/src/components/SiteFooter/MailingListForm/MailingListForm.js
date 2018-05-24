e2bb7cfd7aac1d325b224a395c8d1b1aabb54bf3
joel tong <ec2-user@ip-172-31-5-147.ap-southeast-1.compute.internal>
2018-05-24 07:33:44 +0000

    added in save email funcitonality in mailing list form

--- frontend/src/components/SiteFooter/MailingListForm/MailingListForm.js
+++ frontend/src/components/SiteFooter/MailingListForm/MailingListForm.js
@@ -5,17 +5,34 @@ import { Control, Field, Input, Button, Section, Container } from "bloomer";
 class MailingListForm extends Component {
   constructor() {
     super();
+    this.state = {
+      email: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <Field hasAddons="centered" isPulled="left">
           <Control>
-            <Input isSize="small" />
+            <Input isSize="small" name="email" type="email" isRequired onChange={this.handleChange} />
           </Control>
           <Control>
-            <Button isColor="info" isSize="small">
+            <Button isColor="info" isSize="small" type="submit">
               Register
             </Button>
           </Control>
