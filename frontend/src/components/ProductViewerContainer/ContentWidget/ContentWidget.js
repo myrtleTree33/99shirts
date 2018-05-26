318be07556e210efb18bc8d2dc987f4ff46f4df9
jhtong <me at joeltong dot org>
2018-05-26 23:14:02 +0800

    added in product page

--- frontend/src/components/ProductViewerContainer/ContentWidget/ContentWidget.js
+++ frontend/src/components/ProductViewerContainer/ContentWidget/ContentWidget.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  Container, Section, Title, Image,
-  Level, LevelLeft, LevelRight,
-  Columns, Column, Content
-} from 'bloomer';
+  Container,
+  Section,
+  Title,
+  Image,
+  Level,
+  LevelLeft,
+  LevelRight,
+  Columns,
+  Column,
+  Content,
+  Button,
+  Field,
+  Control,
+  Label,
+  Input
+} from "bloomer";
 
-import './ContentWidget.css';
+import "./ContentWidget.css";
 
 class ContentWidget extends Component {
   constructor() {
@@ -15,24 +27,49 @@ class ContentWidget extends Component {
   render() {
     const { product = {} } = this.props;
     const {
-      id = '0',
-      title = 'Product title',
-      description = 'Product Description',
-      price = '0.00',
-      currency = 'SGD',
-      symbol = '$'
+      id = "0",
+      title = "Product title",
+      description = "Product Description",
+      price = "0.00",
+      currency = "SGD",
+      symbol = "$",
+      sizes = ["S", "M", "L"]
     } = product;
     const formattedPrice = symbol + price;
     return (
-      <Container>
-            <Title isSize="4">{title}</Title>
-            <span className="contentWidget-id">ID #: {id}</span>
-            <Content>
-              <Section>
-                {formattedPrice}
-              </Section>
-            </Content>
-      </Container>
+      <form>
+        <Title isSize="4">{title}</Title>
+        <span className="contentWidget-id">ID #: {id}</span>
+        <Content>
+          <Section>
+            <Label>Size</Label>
+            <Field isGrouped>
+              {sizes.map(s => (
+                <Control>
+                  <Button isColor="dark" isOutlined>
+                    {s.toUpperCase()}
+                  </Button>
+                </Control>
+              ))}
+            </Field>
+            <Field>
+              <Label>Quantity</Label>
+              <Control>
+                <Input
+                  className="contentWidget-qty"
+                  type="text"
+                  placeholder="1"
+                />
+              </Control>
+            </Field>
+          </Section>
+          <Section>
+            <Button type="submit" isSize="medium" isColor="info">
+              Add to cart
+            </Button>
+          </Section>
+        </Content>
+      </form>
     );
   }
 }
--- frontend/src/components/ProductViewerContainer/ProductViewerContainer.js
+++ frontend/src/components/ProductViewerContainer/ProductViewerContainer.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  Container, Section, Title, Image,
-  Columns, Column
-} from 'bloomer';
+  Container,
+  Section,
+  Title,
+  Image,
+  Columns,
+  Column,
+  Level,
+  LevelLeft,
+  LevelRight
+} from "bloomer";
 
-import './ProductViewerContainer.css';
-import ContentWidget from './ContentWidget/ContentWidget';
+import "./ProductViewerContainer.css";
+import ContentWidget from "./ContentWidget/ContentWidget";
 
 class ProductViewer extends Component {
   constructor() {
@@ -15,14 +22,20 @@ class ProductViewer extends Component {
   render() {
     return (
       <Container>
-        <Columns>
-          <Column isSize="1/2" className="productItem-img">
-            <Image isRatio="2:3" src="https://via.placeholder.com/280x420" />
-          </Column>
-          <Column isSize="1/2" hasTextAlign="left" className="productItem-content">
-            <ContentWidget />
-          </Column>
-        </Columns>
+        <Section>
+          <Columns>
+            <Column isSize="1/2" className="productItem-img">
+              <Image isRatio="2:3" src="https://via.placeholder.com/280x420" />
+            </Column>
+            <Column
+              isSize="1/2"
+              hasTextAlign="left"
+              className="productItem-content"
+            >
+              <ContentWidget />
+            </Column>
+          </Columns>
+        </Section>
       </Container>
     );
   }
